bd5ed3cb30ee3e5239a3ca9dce904fe98c4e024f
Jeroen Dalsem <jdalsem@coldtrick.com>
2014-01-15 17:43:23 +0100

    comments are now unlimited listed

--- views/default/object/answer.php
+++ views/default/object/answer.php
@@ -17,32 +17,14 @@ $comment_count = $answer->countComments();
 $comment_options = array(
 		'guid' => $answer->getGUID(),
 		'annotation_name' => 'generic_comment',
-		'limit' => 3,
-		'order_by' => 'n_table.time_created desc'
+		'limit' => false
 );
 
 $comments = elgg_get_annotations($comment_options);
 
 if ($comments) {
-	// why is this reversing it? because we're asking for the 3 latest
-	// comments by sorting desc and limiting by 3, but we want to display
-	// these comments with the latest at the bottom.
-	$comments = array_reverse($comments);
-
 	$body .= "<span class='elgg-river-comments-tab'>" . elgg_echo('comments') . "</span>";
 	$body .= elgg_view_annotation_list($comments, array('list_class' => 'elgg-river-comments'));
-
-	if ($comment_count > count($comments)) {
-		$num_more_comments = $comment_count - count($comments);
-		$url = $object->getURL();
-		$params = array(
-				'href' => $url,
-				'text' => elgg_echo('river:comments:more', array($num_more_comments)),
-				'is_trusted' => true,
-		);
-		$link = elgg_view('output/url', $params);
-		$body .= "<div class=\"elgg-river-more\">$link</div>";
-	}
 }
 
 // show a comment form like in the river
