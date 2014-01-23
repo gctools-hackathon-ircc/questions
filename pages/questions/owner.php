94401471b7537ca92ff1fa3ae6f7b74315e652b7
Jerome Bakker <jeabakker@coldtrick.com>
2014-01-23 09:46:36 +0100

    changed: mine page lists all owned questions

--- pages/questions/owner.php
+++ pages/questions/owner.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Elgg questions plugin everyone page
+ * Elgg questions plugin owner page
  *
  * @package Questions
  */
@@ -13,13 +13,21 @@ elgg_register_title_button();
 
 $title = elgg_echo('questions:owner', array($page_owner->name));
 
-$content = elgg_list_entities(array(
+$options = array(
 	'type' => 'object',
 	'subtype' => 'question',
-	'container_guid' => $page_owner->guid,
 	'full_view' => false,
 	'list_type_toggle' => false
-));
+);
+if (elgg_instanceof($page_owner, "group")) {
+	// groups are containers
+	$options['container_guid'] = $page_owner->getGUID();
+} else {
+	// users list all owned questions
+	$options['owner_guid'] = $page_owner->getGUID();
+}
+
+$content = elgg_list_entities($options);
 
 if (!$content) {
 	$content = elgg_echo('questions:none');
