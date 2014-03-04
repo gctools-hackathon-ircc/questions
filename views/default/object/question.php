3e640ed7abd0ff3d545507c26b69cc7cbc767068
Jerome Bakker <jeabakker@coldtrick.com>
2014-03-04 11:03:41 +0100

    fixed: wrong answer displayed in listing

--- views/default/object/question.php
+++ views/default/object/question.php
@@ -5,8 +5,8 @@
  * @package Questions
 */
 
-$full = elgg_extract('full_view', $vars, false);
-$question = elgg_extract('entity', $vars, false);
+$full = elgg_extract("full_view", $vars, false);
+$question = elgg_extract("entity", $vars, false);
 
 if (!$question) {
 	return true;
@@ -14,9 +14,9 @@ if (!$question) {
 
 $poster = $question->getOwnerEntity();
 
-$poster_icon = elgg_view_entity_icon($poster, 'small');
+$poster_icon = elgg_view_entity_icon($poster, "small");
 $poster_link = elgg_view("output/url", array("text" => $poster->name, "href" => $poster->getURL(), "is_trusted" => true));
-$poster_text = elgg_echo('questions:asked', array($poster_link));
+$poster_text = elgg_echo("questions:asked", array($poster_link));
 
 $container = $question->getContainerEntity();
 if (elgg_instanceof($container, "group") && (elgg_get_page_owner_guid() != $container->getGUID())) {
@@ -24,30 +24,26 @@ if (elgg_instanceof($container, "group") && (elgg_get_page_owner_guid() != $cont
 	$poster_text .= " " . elgg_echo("river:ingroup", array($group_link));
 }
 
-$tags = elgg_view('output/tags', array('tags' => $question->tags));
-$categories = elgg_view('output/categories', $vars);
+$tags = elgg_view("output/tags", array("tags" => $question->tags));
+$categories = elgg_view("output/categories", $vars);
 
 $date = elgg_view_friendly_time($question->time_created);
 
-$answers_link = '';
+$answers_link = "";
 
 $answer_options = array(
-	'type' => 'object',
-	'subtype' => 'answer',
-	'container_guid' => $question->getGUID(),
-	'count' => true,
+	"type" => "object",
+	"subtype" => "answer",
+	"container_guid" => $question->getGUID(),
+	"count" => true,
 );
 
 $num_answers = elgg_get_entities($answer_options);
 $answer_text = "";
 
 if ($num_answers != 0) {
-	$answer_options = array(
-		'limit' => 1,
-		'count' => false,
-	);
-
-	$last_answer = elgg_get_entities($answer_options);
+	$answer_options["limit"] = 1;
+	$answer_options["count"] = false;
 	
 	$correct_answer = $question->getMarkedAnswer();
 
@@ -55,29 +51,31 @@ if ($num_answers != 0) {
 		$poster = $correct_answer->getOwnerEntity();
 		$answer_time = elgg_view_friendly_time($correct_answer->time_created);
 		$poster_text = elgg_view("output/url", array("href" => $poster->getURL(), "text" => $poster->name));
-		$answer_text = elgg_echo('questions:answered:correct', array($poster_text, $answer_time));
+		$answer_text = elgg_echo("questions:answered:correct", array($poster_text, $answer_time));
 	} else {
+		$last_answer = elgg_get_entities($answer_options);
+		
 		$poster = $last_answer[0]->getOwnerEntity();
 		$answer_time = elgg_view_friendly_time($last_answer[0]->time_created);
 		$poster_text = elgg_view("output/url", array("href" => $poster->getURL(), "text" => $poster->name));
-		$answer_text = elgg_echo('questions:answered', array($poster_text, $answer_time));
+		$answer_text = elgg_echo("questions:answered", array($poster_text, $answer_time));
 	}
 	
-	$answers_link = elgg_view('output/url', array(
-		'href' => $question->getURL() . '#question-answers',
-		'text' => elgg_echo('answers') . " ($num_answers)",
+	$answers_link = elgg_view("output/url", array(
+		"href" => $question->getURL() . "#question-answers",
+		"text" => elgg_echo("answers") . " ($num_answers)",
 	));
 }
 
-$metadata = '';
+$metadata = "";
 // do not show the metadata and controls in widget view
-if (!elgg_in_context('widgets')) {
-	$metadata = elgg_view_menu('entity', array(
-		'entity' => $vars['entity'],
-		'handler' => 'questions',
-		'sort_by' => 'priority',
-		'class' => 'elgg-menu-hz',
-		'full_view' => $full
+if (!elgg_in_context("widgets")) {
+	$metadata = elgg_view_menu("entity", array(
+		"entity" => $vars["entity"],
+		"handler" => "questions",
+		"sort_by" => "priority",
+		"class" => "elgg-menu-hz",
+		"full_view" => $full
 	));
 }
 
@@ -97,38 +95,38 @@ if ($full) {
 	$subtitle = "$poster_text $date $answers_link $categories";
 
 	$params = array(
-		'entity' => $question,
-		'title' => false,
-		'metadata' => $metadata,
-		'subtitle' => $subtitle,
-		'tags' => $tags,
+		"entity" => $question,
+		"title" => false,
+		"metadata" => $metadata,
+		"subtitle" => $subtitle,
+		"tags" => $tags,
 	);
-	$list_body = elgg_view('object/elements/summary', $params);
+	$list_body = elgg_view("object/elements/summary", $params);
 	
-	$list_body .= elgg_view('output/longtext', array('value' => $question->description));
+	$list_body .= elgg_view("output/longtext", array("value" => $question->description));
 	
 	$comment_count = $question->countComments();
 	
 	$comment_options = array(
-		'guid' => $question->getGUID(),
-		'annotation_name' => 'generic_comment',
-		'limit' => false
+		"guid" => $question->getGUID(),
+		"annotation_name" => "generic_comment",
+		"limit" => false
 	);
 	$comments = elgg_get_annotations($comment_options);
 	
 	if ($comments) {
-		$list_body .= "<span class='elgg-river-comments-tab'>" . elgg_echo('comments') . "</span>";
-		$list_body .= elgg_view_annotation_list($comments, array('list_class' => 'elgg-river-comments'));
+		$list_body .= "<span class='elgg-river-comments-tab'>" . elgg_echo("comments") . "</span>";
+		$list_body .= elgg_view_annotation_list($comments, array("list_class" => "elgg-river-comments"));
 	}
 	
 	// show a comment form like in the river
 	$body_vars = array(
-		'entity' => $question,
-		'inline' => true
+		"entity" => $question,
+		"inline" => true
 	);
 	
 	$list_body .= "<div class='elgg-river-item hidden' id='comments-add-" . $question->getGUID() . "'>";
-	$list_body .= elgg_view_form('comments/add', array(), $body_vars);
+	$list_body .= elgg_view_form("comments/add", array(), $body_vars);
 	$list_body .= "</div>";
 	
 	echo elgg_view_image_block($poster_icon, $list_body);
@@ -149,14 +147,14 @@ if ($full) {
 	$subtitle = "$poster_text $date $answers_link $categories";
 
 	$params = array(
-		'entity' => $question,
-		'title' => $title,
-		'metadata' => $metadata,
-		'subtitle' => $subtitle,
-		'tags' => $tags,
-		'content' => $answer_text
+		"entity" => $question,
+		"title" => $title,
+		"metadata" => $metadata,
+		"subtitle" => $subtitle,
+		"tags" => $tags,
+		"content" => $answer_text
 	);
-	$list_body = elgg_view('object/elements/summary', $params);
+	$list_body = elgg_view("object/elements/summary", $params);
 
 	echo elgg_view_image_block($poster_icon, $list_body);
 }
